edcad8d82a743b15ff565eb3fb63a7758bc9fb12
Walter <walter.laborde@hotmail.com>
2022-10-30 19:31:07 -0300

    SUT usuario

---
+++ test/ar/edu/unq/po2/clasesBasicas/UsuarioTestCase.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.clasesBasicas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
