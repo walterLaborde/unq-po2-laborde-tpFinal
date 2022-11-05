a17ced2ef124ff741f879b40e0ebd15112a6ac7d
Walter <walter.laborde@hotmail.com>
2022-11-05 09:12:46 -0300

    clase restriccion geografica

---
+++ src/ar/edu/unq/po2/clasesBasicas/RestriccionGeografica.java
@@ -0,0 +1,47 @@
+package ar.edu.unq.po2.clasesBasicas;
+
+
+//import ar.edu.unq.po2.tpfinal.Coordenada;
+
+public class RestriccionGeografica {
+
+	private int latitud;
+	private int longitud;
+	private int radio;
+
+	public RestriccionGeografica(int latitud, int longitud, int radio) {
+		this.setLatitud(latitud);
+		this.setLongitud(longitud);
+		this.setRadio(radio);
+	}
+
+	private int getLatitud() {
+		return this.latitud;
+	}
+
+	private void setLatitud(int latitud) {
+		this.latitud = latitud;
+	}
+
+	private int getLongitud() {
+		return this.longitud;
+	}
+
+	private void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+
+	public int getRadio() {
+		return this.radio;
+	}
+
+	private void setRadio(int radio) {
+		this.radio = radio;
+	}
+
+	public boolean contiene(Coordenada coordenada) {
+		return Math.sqrt(Math.pow(Math.abs(coordenada.getX() - this.getLatitud()), 2)
+				+ Math.pow(Math.abs(coordenada.getY() - this.getLongitud()), 2)) < this.getRadio();
+	}
+}
+}
